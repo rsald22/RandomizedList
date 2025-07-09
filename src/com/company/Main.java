548ea56bab30d2f04ef236b8e3106683a9f9e3f8
rsald22 <99000329+rsald22@users.noreply.github.com>
2025-07-08 21:10:53 -0400

    open method
    created the logic (untested) for open method. Needs to be moved to dedicated method in masterlist class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,12 +9,6 @@ public class Main {
 
         setup();
 
-        /*
-        for (int i = 0; i < ml.size(); i++){
-            System.out.println(ml.get(i).getName());
-        }
-        */
-
         System.out.println("Welcome to Randomized List");
         System.out.println("Do you want to:\nOPEN a list\nCREATE a new list\nEDIT an existing" +
                 " list\nDELETE a list");
@@ -40,14 +34,48 @@ public class Main {
     }
 
     public static void createList(){
+        String name = "";
         System.out.println("What type of list do you want to make?" +
                 "\nSTRING or INTEGER");
         Scanner input = new Scanner(System.in);
         String type = input.nextLine();
-        System.out.println("What do you want to name this list?");
-        String name = input.nextLine();
-        ml.add(new RandomList(type, name));
+        boolean nameOption = false;
 
+        /*
+            THIS BIT SHOULD BE IN A METHOD IN THE MASTERLIST FUNCTION.
+         */
+        while (nameOption == false) {
+
+            if (ml.size() == 0){
+                System.out.println("What do you want to name this list?");
+                name = input.nextLine();
+                ml.add(new RandomList(type, name));
+            } else {
+                for (int i = 0; i < ml.size(); i++) {
+                    if (ml.get(i).getName().equals(name)) {
+                        System.out.println("Name already in use");
+                        break;
+                    } else {
+                        ml.add(new RandomList(type, name));
+                        nameOption = true;
+                    }
+                }
+            }
+        }
+
+        System.out.println("List successfully created");
+        System.out.println("Do you want to add to this list?");
+        String selection = input.nextLine();
+        if (selection.equalsIgnoreCase("y") || selection.equalsIgnoreCase("yes")){
+            edit(name);
+        }
+    }
+
+    public static void edit(String listName){
+        /*
+            Have the editting / adding bit in the random list class
+            but use this side for input getting + accessing the masterlist as needed?
+         */
     }
 
 }
